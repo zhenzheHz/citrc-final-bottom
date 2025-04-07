f513f045c1d0798bd026be3bbcd1c02ee1b839fc
zhenzhe <165050331+zhenzheHz@users.noreply.github.com>
2025-04-08 00:57:14 +0800

    feat : add the solution code to problem H

---
+++ Solutions/H.Goodbye/AC.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+int main() {
+    std::string ans[16] = {
+        "mingyee",
+        "Goodbye!\\><\/",
+        "hello, world",
+        "04/30",
+        "zhenzhe",
+        "hanks2017",
+        "sh1ng",
+        "citrc",
+        "2",
+        "4",
+        "7",
+        "26",
+        "3",
+        "224",
+        "4",
+        "kita ikuyo"
+    };
+    int q;
+    std::cin>>q;
+    return std::cout<<ans[q],0;
+}
