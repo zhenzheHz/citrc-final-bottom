d424cebb96e2c6b4fffe468b0fd9cfca2cc8cc9f
zhenzhe <165050331+zhenzheHz@users.noreply.github.com>
2025-05-16 21:22:35 +0800

    feat : add the rand funcion in tools

--- Generator/tool.py
+++ Generator/tool.py
@@ -1,4 +1,5 @@
 import os
+import random
 def conv(x):
     s = str(x)
     while len(s) < 3: s = '0' + s
@@ -7,8 +8,8 @@ def conv(x):
 def compile(x):
     os.system(f"g++ -std=c++17 -o sol {x}")
 
-def duplicate(src):
-    for i in range(100):
+def duplicate(src, x = 0, y = 100):
+    for i in range(x,y):
         j = conv(i)
         with open(f"{src}/{j}.in", "r") as cin, open(f"{i+1}.in", "w") as out:
             for line in cin:
@@ -20,3 +21,7 @@ def duplicate(src):
 
 def delete(src):
     os.system(f"rm {src}")
+
+
+def Random(a,b):
+    return random.randint(a,b)
