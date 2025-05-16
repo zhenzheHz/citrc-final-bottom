70d3a93075107707dd57279ea1c21f394b0258a6
zhenzhe <165050331+zhenzheHz@users.noreply.github.com>
2025-05-16 20:42:43 +0800

    del : the original wrong solution

--- Solutions/E.Today's Kahoot/Sub1.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-#define int int64_t
-signed main() {
-    int q,a,b,p;
-    std::cin >> q >> a >> b >> p;
-    assert(q == 1 && a == 2 && p == 10);
-    int ans[] = {6,2,4,8};
-    return std::cout<<ans[b%4],0;
-}
